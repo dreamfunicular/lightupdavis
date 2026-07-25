49f0dbc275bb96d980eddebff4c1c841456a9240
Anna Lynch <ahp@unc.edu>
2026-07-25 01:56:52 -0400

    Chronos hates me, testing for two events in sequence

--- worker/process_queue_test.go
+++ worker/process_queue_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -16,11 +17,58 @@ func TestSingleEntryQueue(t *testing.T) {
 
 	mockHandler := func(e UpdateBrightnessEvent) {
 		if e != q[0] {
-			t.Errorf("Unexpected call")
+			t.Error("Actual call to handler did not match expectation")
 		}
 	}
 
 	ch := make(chan UpdateBrightnessEvent, 100)
 
 	processQueue(mockHandler, q, ch)
+}
+
+func TestTwoEntryQueue(t *testing.T) {
+	curr := time.Now()
+	expectedOrder := []UpdateBrightnessEvent{
+		{
+			timestamp:  curr.Add(300 * time.Millisecond),
+			bulbNo:     1,
+			brightness: .1,
+		},
+		{
+			timestamp:  curr.Add(400 * time.Millisecond),
+			bulbNo:     1,
+			brightness: .1,
+		},
+	}
+
+	var actualOrder []UpdateBrightnessEvent = make([]UpdateBrightnessEvent, 0)
+
+	mockHandler := func(e UpdateBrightnessEvent) {
+		actualOrder = append(actualOrder, e)
+	}
+
+	q := []UpdateBrightnessEvent{
+		{
+			timestamp:  curr.Add(300 * time.Millisecond),
+			bulbNo:     1,
+			brightness: .1,
+		},
+		{
+			timestamp:  curr.Add(400 * time.Millisecond),
+			bulbNo:     1,
+			brightness: .1,
+		},
+	}
+
+	ch := make(chan UpdateBrightnessEvent, 100)
+
+	processQueue(mockHandler, q, ch)
+
+	if len(expectedOrder) != len(actualOrder) {
+		t.Errorf("Expected %d calls to handler; got %d.\n", len(expectedOrder), len(actualOrder))
+	}
+
+	if !reflect.DeepEqual(expectedOrder, actualOrder) {
+		t.Error("Contents of expected and actual calls to handler differed.")
+	}
 }
