af97243a9f9b6d95d6c8db6640eb10aad9d42b61
Anna Lynch <ahp@unc.edu>
2026-07-26 22:05:49 -0400

    Added fixtures for update and queue generation; added tests for fixtures

--- worker/process_queue_test.go
+++ worker/process_queue_test.go
@@ -7,6 +7,80 @@ import (
 	"time"
 )
 
+func generateUpdateBrightnessEvent(t time.Time, i int) (e UpdateBrightnessEvent) {
+	return UpdateBrightnessEvent{
+		timestamp:  t,
+		bulbNo:     1,
+		brightness: float32(i) * 0.1,
+	}
+}
+
+func TestGenerateUpdateBrightnessEvent(t *testing.T) {
+	curr := time.Now()
+	expected := UpdateBrightnessEvent{
+		timestamp:  curr,
+		bulbNo:     1,
+		brightness: 0.1,
+	}
+	actual := generateUpdateBrightnessEvent(curr, 1)
+
+	if !reflect.DeepEqual(expected, actual) {
+		t.Error("Unequal!")
+	}
+}
+
+func generateQueue(curr time.Time, start int, len int) (q []UpdateBrightnessEvent) {
+	for i := start; i < start+len; i++ {
+		newTime := curr.Add(time.Duration(i) * time.Millisecond)
+		new := generateUpdateBrightnessEvent(newTime, i)
+		q = append(q, new)
+	}
+
+	return q
+}
+
+func TestGenerateQueue(t *testing.T) {
+	curr := time.Now()
+
+	expected := []UpdateBrightnessEvent{
+		{
+			timestamp:  curr.Add(1 * time.Millisecond),
+			bulbNo:     1,
+			brightness: .1,
+		},
+		{
+			timestamp:  curr.Add(2 * time.Millisecond),
+			bulbNo:     1,
+			brightness: .2,
+		},
+		{
+			timestamp:  curr.Add(3 * time.Millisecond),
+			bulbNo:     1,
+			brightness: .3,
+		},
+	}
+
+	actual := generateQueue(curr, 1, 3)
+
+	if len(expected) != len(actual) {
+		t.Errorf("Expected %d calls to handler; got %d.\n", len(expected), len(actual))
+	}
+
+	for i := range len(expected) {
+		if expected[i].timestamp != actual[i].timestamp {
+			t.Errorf("Timestamp mismatch")
+		}
+
+		if expected[i].brightness != actual[i].brightness {
+			t.Errorf("brightness mismatch")
+		}
+
+		if expected[i].bulbNo != actual[i].bulbNo {
+			t.Errorf("bulbNo mismatch")
+		}
+	}
+}
+
 func TestSingleEntryQueue(t *testing.T) {
 	q := []UpdateBrightnessEvent{
 		{
