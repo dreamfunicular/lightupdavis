1aa4b85c1f7b531595856ba33f3cfce492b56e51
Anna Lynch <ahp@unc.edu>
2026-08-04 00:25:13 -0400

    Added context-based expiration

--- worker/main.go
+++ worker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"time"
 )
@@ -18,40 +19,30 @@ func handleUpdateBrightnessEvent(e UpdateBrightnessEvent) {
 }
 
 func processQueue(handler UpdateBrightnessEventHandler, q []UpdateBrightnessEvent, ch chan UpdateBrightnessEvent) {
-	for len(q) > 0 {
-		e := q[0]
+	ctx, cancel := context.WithTimeout(context.Background(), 1500*time.Millisecond)
+	defer cancel()
 
-		if e.timestamp.Compare(time.Now()) == -1 {
+	for {
+		for len(q) > 0 {
+			e := q[0]
+
+			if e.timestamp.Compare(time.Now()) == -1 {
+				q = q[1:]
+				continue
+			}
+
+			go func() {
+				time.Sleep(time.Until(e.timestamp))
+				handler(e)
+			}()
 			q = q[1:]
-			continue
 		}
 
-		time.Sleep(time.Until(e.timestamp))
-		// TODO: Leaves the possibility of a new, more urgent, instruction arriving while the current upcoming instruction is waiting.
-		handler(e)
-		q = q[1:]
-
-		// Non-blocking poll of the channel
 		select {
 		case new := <-ch:
-			fmt.Println("looping")
-			// Catches all buffer-created, late, or bugged updates.
-			if new.timestamp.Before(time.Now()) {
-				continue
-			}
-
-			// TODO: Leaves the possibility of new, more urgent, instructions being behind other ones in the queue. Needs to iterate through all additons.
 			q = append(q, new)
-			for each := range ch {
-				q = append(q, each)
-			}
-		default:
-			if len(q) > 0 {
-				continue
-			} else {
-				new := <-ch
-				q = append(q, new)
-			}
+		case <-ctx.Done():
+			return
 		}
 	}
 }
