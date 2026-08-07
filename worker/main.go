341788a5532bd160990eafa1b05c32ae3b2609d1
Anna Lynch <ahp@unc.edu>
2026-08-07 01:44:28 -0400

    Both tests pass independently but conflict when run in sequence

--- worker/main.go
+++ worker/main.go
@@ -2,18 +2,41 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
 
-func runServer(ctx context.Context, handler func(w http.ResponseWriter, r *http.Request)) {
+func startServer(ctx context.Context, handler func(w http.ResponseWriter, r *http.Request)) {
+	// http.HandleFunc("/", handler)
+	// log.Fatal(http.ListenAndServe(":8080", nil))
+	srv := &http.Server{Addr: ":8080"}
 	http.HandleFunc("/", handler)
-	http.ListenAndServe("localhost:8080", nil)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			fmt.Println("Error caused server to fail:", err)
+		}
+	}()
+
+	<-ctx.Done()
+	err := srv.Shutdown(ctx)
+
+	if err != nil {
+		fmt.Println("error ocurred")
+	}
 }
 
-func main() {
-	// MVP Project
-	// Create channel
-	// Spin up proccess queue that reads from channel
-	// Create handler function that writes to channel
-	// Spin up server with handler function
-}
+// func main() {
+// 	ctx, cancel := context.WithCancel(context.Background())
+
+// 	go startServer(ctx, func(w http.ResponseWriter, r *http.Request) { fmt.Println("Received incoming request") })
+
+// 	time.Sleep(100 * time.Millisecond)
+// 	cancel()
+// 	//		// MVP Project
+// 	//		// Create channel
+// 	//		// Spin up proccess queue that reads from channel
+// 	//		// Create handler function that writes to channel
+// 	//		// Spin up server with handler function
+// 	//	}
+// }
--- worker/main_test.go
+++ worker/main_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"reflect"
+	"sync"
 	"testing"
 	"time"
 )
@@ -38,40 +39,33 @@ func generateQueue(curr time.Time, start int, len int) (q []UpdateBrightnessEven
 }
 
 func TestPing(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
-	defer cancel()
+	ctx, cancel := context.WithCancel(context.Background())
 
-	handlerCalled := false
+	called := false
 
-	pingHandler := func(w http.ResponseWriter, r *http.Request) {
-		handlerCalled = true
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		called = true
 	}
 
-	go runServer(ctx, pingHandler)
+	var wg sync.WaitGroup
+	wg.Go(func() { startServer(ctx, handler) })
 
-	time.Sleep(20 * time.Millisecond)
+	time.Sleep(50 * time.Millisecond)
 
-	r, err := http.NewRequest("POST", "http://localhost:8080", nil)
-	if err != nil {
-		t.Error(err)
-		os.Exit(1)
+	http.Post("http://localhost:8080/", "application/json", nil)
+
+	time.Sleep(50 * time.Millisecond)
+	cancel()
+
+	if !called {
+		t.Errorf("Ping failed")
 	}
-	_, err = http.DefaultClient.Do(r)
-	if err != nil {
-		t.Error(err)
-		os.Exit(1)
-	}
 
-	time.Sleep(20 * time.Millisecond)
-
-	if !handlerCalled {
-		t.Errorf("Failed to ping the server")
-	}
+	wg.Wait()
 }
 
 func TestOneMessage(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
-	defer cancel()
+	ctx, cancel := context.WithCancel(context.Background())
 
 	curr := time.Now().Truncate(0)
 
@@ -100,7 +94,7 @@ func TestOneMessage(t *testing.T) {
 		actual = append(actual, e)
 	}
 
-	go runServer(ctx, handler)
+	go startServer(ctx, handler)
 
 	e := expected[0]
 	body, err := json.Marshal(e)
@@ -115,6 +109,7 @@ func TestOneMessage(t *testing.T) {
 	http.Post("http://localhost:8080/", "application/json", bytes.NewBuffer(body))
 
 	time.Sleep(50 * time.Millisecond)
+	cancel()
 
 	if !reflect.DeepEqual(expected, actual) {
 		t.Error("Contents of expected and actual calls to handler differed.")
